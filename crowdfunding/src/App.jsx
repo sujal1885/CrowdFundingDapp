5da288cf956ef022bc2e524ec47eae6556b65df6
sujal <sujalagrawalgondia@gmail.com>
2023-04-10 01:00:04 +0530

    created basic layout

--- crowdfunding/src/App.jsx
+++ crowdfunding/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/header"
 import Home from "./components/Home" 
+import About from "./components/About"
+import Founders from "./components/Founders"
 
 function App () {
     
@@ -7,6 +9,8 @@ function App () {
         <>
         <Header/>
         <Home/>
+        <About />
+        <Founders />
         </>
     )
 }
