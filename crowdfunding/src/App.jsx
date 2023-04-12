bd3ae2f06de771995a8b3ed2ec27b2e7e8e92b9e
Vivek <vivek.meher303@gmail.com>
2023-04-12 10:10:19 +0530

    burger button added

--- crowdfunding/src/App.jsx
+++ crowdfunding/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header"
+import Header from "./components/Header"
 import Home from "./components/Home" 
 import About from "./components/About"
 import Founders from "./components/Founders"
--- crowdfunding/src/components/Header.jsx
+++ crowdfunding/src/components/Header.jsx
@@ -1,27 +1,45 @@
+import { Component } from "react";
 import "./page.css";
 
-function Header (){
-    return (
-        <>
-            <body>
-                <header>
-                    <div>
-                        <a href="">CWFunD</a>
-                    </div>
-                    <div>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                            <li>Donate</li>
-                        </ul>
-                    </div>
-                </header>
-            </body>
+class Header extends Component{
 
-        </>
-        
-    )
+    state ={ clicked:false};
+    handleClick = () => {
+        this.setState( {clicked:!this.state.clicked})
+    }
     
-}
-export default Header
+    render(){
+        return (
+            <>
+                <body>
+                    <header>
+                        <div>
+                            <a href="">CWFunD</a>
+                        </div>
+                        <div>
+                            <ul 
+                                className={!this.state.clicked ? "ul active":"ul" }
+                                >
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                                <li>Donate</li>
+                            </ul>
+                        </div>
+                        <div id="mobile" onClick={this.handleClick}>
+                            <id id="bar"
+                                className={
+                                    this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+                                    ></id>
+                        </div>
+
+                    </header>
+                </body>
+
+            </>
+            
+            )
+            
+        }
+    }
+    export default Header
