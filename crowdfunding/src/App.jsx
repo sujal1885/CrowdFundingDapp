0bf4bb0694be8a38bbbc12a93927a98e79681745
sujal1885 <103549709+sujal1885@users.noreply.github.com>
2023-04-14 14:51:38 +0530

    Update App.jsx

--- crowdfunding/src/App.jsx
+++ crowdfunding/src/App.jsx
@@ -1,22 +1,18 @@
-import Header from "./components/Header"
-import Home from "./components/Home" 
-import About from "./components/About"
-import Founders from "./components/Founders"
-import Footer from "./components/Footer"
+import Header from "../components/Header"
+import Home from "../components/Home" 
+import About from "../components/About"
+import Founders from "../components/Founders"
+import Footer from "../components/Footer"
 
 function App () {
     
     return (
         <>
-        <Header/>
         <Home/>
         <About />
         <Founders />
         <Footer />
-
-        
         </>
-
     )
 }
 export default App
