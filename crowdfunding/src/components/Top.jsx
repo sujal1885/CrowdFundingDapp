23a6eb6ae687b25d8833fe3bc5d99e48e59872d6
Kartik <whonalekartik11@gmail.com>
2023-04-15 22:47:29 +0530

    change form.css

---
+++ crowdfunding/src/components/Top.jsx
@@ -0,0 +1,19 @@
+import "./top.css";
+
+function Top() {
+    return (
+        <Box
+            sx={{
+                background: '#1F1F1F',
+                paddingLeft: '3%',
+                paddingRight: '3%',
+                paddingTop: '0.5%',
+                paddingBottom: '5%',
+            }}
+        >
+            <p>hi</p>
+        </Box>  
+    )
+
+}
+export default Top
